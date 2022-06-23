98136b5fd863e7bf12980040ab525363678bf6d8
cheecrma <cheecrma@gmail.com>
2022-06-23 22:16:38 +0900

    update vue basic re

---
+++ 1_Vue_Summary_0/vue_start_practice_replay/03_function.js
@@ -0,0 +1,17 @@
+const member = {
+  name: 'cheecrma',
+  age: '27',
+  sId: 201612383,
+}
+
+const member2 = {
+  name: 'henry',
+  age: '20',
+  sId: 200012383,
+}
+
+function Member(name, age, sId){
+  this.name = name
+  this.age = age
+  this.sId = sId
+}
